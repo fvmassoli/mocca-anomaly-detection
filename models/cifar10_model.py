ff18914ccafb7d2fa26cb4907ab5a1d0b938874e
fvmassoli <fabiovaleriomassoli@gmail.com>
2021-01-29 08:57:25 +0100

    implemented models

---
+++ models/cifar10_model.py
@@ -0,0 +1,187 @@
+import logging
+import numpy as np
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+def init_conv(out_channels: int, k_size: int = 5):
+    """ Init convolutional layers.
+
+    Parameters
+    ----------
+    k_size : int
+        Kernel size
+    out_channels : int
+        Output features size
+
+    """
+    l = nn.Conv2d(
+            in_channels=3 if out_channels==32 else out_channels//2, 
+            out_channels=out_channels, 
+            kernel_size=k_size, 
+            bias=False, 
+            padding=2
+        )
+    nn.init.xavier_uniform_(l.weight, gain=nn.init.calculate_gain('leaky_relu'))
+    return l
+    
+
+def init_deconv(out_channels: int, k_size: int = 5):
+    """ Init deconv layers.
+
+    Parameters
+    ----------
+    k_size : int
+        Kernel size
+    out_channels : int
+        Input features size
+
+    """
+    l = nn.ConvTranspose2d(
+            in_channels=out_channels, 
+            out_channels=3 if out_channels==32 else out_channels//2, 
+            kernel_size=k_size, 
+            bias=False, 
+            padding=2
+        )
+    nn.init.xavier_uniform_(l.weight, gain=nn.init.calculate_gain('leaky_relu'))
+    return l
+
+
+class BaseNet(nn.Module):
+    """Base class for all neural networks.
+    
+    """
+    def __init__(self):
+        super().__init__()
+
+        # init Logger to print model infos
+        self.logger = logging.getLogger(self.__class__.__name__)
+        
+        # List of input/output features depths for the convolutional layers
+        self.output_features_sizes = [32, 64, 128]
+    
+    def __init_bn(self, num_features: int):
+            """ Init BatchNorm layers.
+
+            Parameters
+            ----------
+            num_features : int
+                Number of input features
+
+            """
+            return nn.BatchNorm2d(num_features=num_features, eps=1e-04, affine=False)
+
+    def summary(self):
+        """Network summary.
+        
+        """
+        net_parameters = filter(lambda p: p.requires_grad, self.parameters())
+        params = sum([np.prod(p.size()) for p in net_parameters])
+        self.logger.info('Trainable parameters: {}'.format(params))
+        self.logger.info(self)
+
+
+class CIFAR10_Encoder(BaseNet):
+    """"Encoder network.
+    
+    """
+    def __init__(self, code_length: int):
+        """"Init encoder.
+
+        Parameters
+        ----------
+        code_length : int
+            Latent code size
+        
+        """
+        super().__init__()
+        
+        # Init Conv layers
+        self.conv1, self.conv2, self.conv3 = [init_conv(out_channels) for out_channels in self.output_features_sizes]
+        
+        # Init BN layers
+        self.bnd1, self.bnd2, self.bnd3 = [self.__init_bn(num_features) for num_features in self.output_features_sizes]
+
+        # Init all other layers
+        self.pool = nn.MaxPool2d(kernel_size=2, stride=2)
+        self.fc1 = nn.Linear(in_features=128 * 4 * 4, out_features=code_length, bias=False)
+    
+    def forward(self, *input: Any):
+        x = self.conv1(input)
+        x = self.pool(F.leaky_relu(self.bnd1(x)))
+        x = self.conv2(x)
+        x = self.pool(F.leaky_relu(self.bnd2(x)))
+        x = self.conv3(x)
+        x = self.pool(F.leaky_relu(self.bnd3(x)))
+        x = self.fc1(x.view(x.size(0), -1))
+        return x
+
+
+class CIFAR10_Decoder(BaseNet):
+    """Full Decoder network.
+
+    """ 
+    def __init__(self, code_length: int):
+        """Init decoder.
+
+        Parameters
+        ----------
+        code_length : int
+            Latent code size
+
+        """
+        super().__init__()
+
+        self.rep_dim = code_length
+        
+        # Build the Decoder
+        self.deconv1 = nn.ConvTranspose2d(int(self.rep_dim / (4 * 4)), 128, 5, bias=False, padding=2)
+        self.deconv2, self.deconv3, self.deconv4 = [init_deconv(out_channels) for out_channels in self.output_features_sizes[::-1]]
+
+        # Init BN layers
+        self.bnd4, self.bnd5, self.bnd6 = [self.__init_bn(num_features) for num_features in self.output_features_sizes[::-1]] 
+
+    def forward(self, *input: Any):
+        x = self.bn1d(input)
+        x = x.view(x.size(0), int(self.rep_dim / (4 * 4)), 4, 4)
+        x = F.leaky_relu(x)
+        x = self.deconv1(x)
+        x = F.interpolate(F.leaky_relu(self.bnd4(x)), scale_factor=2)
+        x = self.deconv2(x)
+        x = F.interpolate(F.leaky_relu(self.bnd5(x)), scale_factor=2)
+        x = self.deconv3(x)
+        x = F.interpolate(F.leaky_relu(self.bnd6(x)), scale_factor=2)
+        x = self.deconv4(x)
+        x = torch.sigmoid(x)
+        return x
+
+
+class CIFAR10_Autoencoder(BaseNet):
+    """Full AutoEncoder network.
+
+    """ 
+    def __init__(self, code_length: int = 128):
+        """Init the AutoEncoder 
+
+        Parameters
+        ----------
+        code_length : int
+            Latent code size
+        
+        """
+        super().__init__()
+        
+        # Build the Encoder
+        self.encoder = CIFAR10_Encoder(code_length=code_length)
+        self.bn1d = nn.BatchNorm1d(num_features=code_length, eps=1e-04, affine=False)
+
+        # Build the Decoder
+        self.decoder = CIFAR10_Decoder(code_length=code_length)
+        
+    def forward(self, *input: Any):
+        z = self.encoder(input)
+        z = self.bn1d(z)
+        return self.decoder(z)
---
+++ models/mvtec_model.py
@@ -0,0 +1,281 @@
+import sys
+from operator import mul
+from typing import Tuple
+from functools import reduce
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+from mvtec_base_model import BaseModule, DownsampleBlock, ResidualBlock, UpsampleBlock
+
+
+CHANNELS = [32, 64, 128]
+
+
+def init_conv_blocks(channel_in: int, channel_out: int, activation_fn: nn):
+            """ Init convolutional layers.
+
+            Parameters
+            ----------
+            k_size : int
+                Kernel size
+            out_channels : int
+                Output features size
+
+            """
+            return DownsampleBlock(channel_in=channel_in, channel_out=channel_out, activation_fn=self.activation_fn)
+
+
+class Selector(nn.Module):
+    """Selector module
+
+    """
+    def __init__(self, code_length: int, idx: int):
+        super().__init__()
+        """Init Selector architeture
+
+        Parameters
+        ----------
+        code_length : int 
+            Latent code size
+        idx : int
+            Layer idx
+        
+        """
+        # List of depths of features maps
+        sizes = [CHANNELS[0], CHANNELS[0], CHANNELS[1], CHANNELS[2], CHANNELS[2]*2, CHANNELS[2]*2, code_length]
+        
+        # Hidden FC output size
+        mid_features_size = 256
+
+        # Last FC output size
+        out_features = 128
+
+        # Choose a different Selector architecture
+        # depending on which layer it attaches
+        if idx < 5:
+            self.fc = nn.Sequential(
+                            nn.AdaptiveMaxPool2d(output_size=8),
+                            nn.Conv2d(in_channels=sizes[idx], out_channels=1, kernel_size=1),
+                            nn.Flatten(),
+                            nn.Linear(in_features=8**2, out_features=mid_features_size, bias=True),
+                            nn.BatchNorm1d(mid_features_size),
+                            nn.ReLU(),
+                            nn.Linear(in_features=mid_features_size, out_features=out_features, bias=True)
+                        )
+        else:
+            self.fc = nn.Sequential(
+                            nn.Flatten(),
+                            nn.Linear(in_features=sizes[idx],  out_features=mid_features_size, bias=True),
+                            nn.BatchNorm1d(mid_features_size),
+                            nn.ReLU(),
+                            nn.Linear(in_features=mid_features_size, out_features=out_features, bias=True)
+                        )
+
+    def forward(self, *input: Any):
+        return self.fc(input)
+
+
+class MVtecEncoder(BaseModule):
+    """MVtec Encoder network
+    
+    """
+    def __init__(self, input_shape: torch.Tensor, code_length: int, idx_list_enc: list, use_selectors: bool):
+        """Init Encoder network
+        
+        Parameters
+        ----------
+        input_shape : torch.Tensor
+            Input data shape
+        code_length : int
+            Latent code size
+        idx_list_enc : list 
+            List of layers' idx to use for the AD task
+        use_selectors : bool
+            True (False) if the model has (not) to use Selectors modules
+
+        """
+        super().__init__()
+
+        # List of layers' idx to use for the AD task
+        self.idx_list_enc = idx_list_enc
+
+        # True (False) if the model has (not) to use Selectors modules
+        self.use_selectors = use_selectors
+
+        # Single input data shape
+        c, h, w = input_shape
+
+        # Activation function
+        self.activation_fn = nn.LeakyReLU()
+
+        # Init convolutional blocks
+        self.conv = nn.Conv2d(in_channels=c, out_channels=32, kernel_size=3, bias=False)
+        self.res  = ResidualBlock(channel_in=32, channel_out=32, activation_fn=self.activation_fn)
+        self.dwn1, self.dwn2, self.dwn3 = [init_conv_blocks(channel_in=ch, channel_out=ch*2, activation_fn=self.activation_fn) for ch in CHANNELS] 
+        
+        # Depth of the last features map
+        self.last_depth = CHANNELS[2]*2
+
+        # Shape of the last features map
+        self.deepest_shape = (self.last_depth, h // 8, w // 8) 
+        
+        # init FC layers
+        self.fc1 = nn.Linear(in_features=reduce(mul, self.deepest_shape), out_features=self.last_depth)
+        self.bn  = nn.BatchNorm1d(num_features=self.last_depth)
+        self.fc2 = nn.Linear(in_features=self.last_depth, out_features=code_length)
+        
+        ## Init features selector models
+        if self.use_selectors:
+            self.selectors = nn.ModuleList([Selector(code_length=code_length, idx=idx) for idx in range(7)])
+            self.selectors.append(Selector(code_length=code_length, idx=6))
+
+    def get_depths_info(self):
+        """
+        Return
+        ------
+        self.last_depth : int
+            Depth of the last features map
+        self.deepest_shape : int
+            Shape of the last features map
+
+        """
+        return self.last_depth, self.deepest_shape
+
+    def forward(self, *input: Any):
+        o1 = self.conv(input)
+        o2 = self.res(self.activation_fn(o1))
+        o3 = self.dwn1(o2)
+        o4 = self.dwn2(o3)
+        o5 = self.dwn3(o4)
+        o7 = self.activation_fn(
+                            self.bn(
+                                self.fc1(
+                                    o5.view(len(o5), -1)
+                                    )
+                                )
+                            ) # FC -> BN -> LeakyReLU
+        o8 = self.fc2(o7)
+        z = nn.Sigmoid()(o8)
+        
+        outputs = [o1, o2, o3, o4, o5, o7, o8, z]
+
+        if self.use_selectors:
+            tuple_o = [self.selectors[idx](tt) for idx, tt in enumerate(outputs) if idx in self.idx_list_enc]
+        else:
+            tuple_o = []
+            for idx, tt in enumerate(outputs):
+                if idx not in self.idx_list_enc: continue
+                if tt.ndimension() > 2:
+                    tuple_o.append(F.avg_pool2d(tt, tt.shape[-2:]).squeeze())
+                else:
+                    tuple_o.append(tt.squeeze())
+
+        names = [f'0{idx}' for idx in range(len(outputs)) if idx in self.idx_list_enc]
+        zipped = list(zip(names, tuple_o))
+
+        return zipped[i] if len(self.idx_list_enc) != 0 else z
+
+
+class MVTecDecoder(BaseModule):
+    """MVTec Decoder network
+    
+    """
+    def __init__(self, code_length: int, deepest_shape: int, last_depth: int, output_shape: torch.Tensor):
+        """Init MVtec Decoder network
+        
+        Parameters
+        ----------
+        code_length : int
+            Latent code size
+        deepest_shape : int
+            Depth of the last encoder features map
+        output_shape : torch.Tensor
+            Input Data shape
+        
+        """
+        super().__init__()
+
+        # Latent code size
+        self.code_length = code_length
+        
+        # deepest_shape
+        self.deepest_shape = deepest_shape
+        
+        # output_shape
+        self.output_shape = output_shape
+
+        # Decoder activation function
+        activation_fn = nn.LeakyReLU()
+
+        # FC network
+        self.fc = nn.Sequential(
+            nn.Linear(in_features=code_length, out_features=last_depth),
+            nn.BatchNorm1d(num_features=last_depth),
+            activation_fn,
+            nn.Linear(in_features=last_depth, out_features=reduce(mul, deepest_shape)),
+            nn.BatchNorm1d(num_features=reduce(mul, deepest_shape)),
+            activation_fn
+        )
+
+        # (Transposed) Convolutional network
+        self.conv = nn.Sequential(
+            UpsampleBlock(channel_in=CHANNELS[2]*2, channel_out=CHANNELS[2], activation_fn=activation_fn),
+            UpsampleBlock(channel_in=CHANNELS[1]*2, channel_out=CHANNELS[1], activation_fn=activation_fn),
+            UpsampleBlock(channel_in=CHANNELS[0]*2, channel_out=CHANNELS[0], activation_fn=activation_fn),
+            ResidualBlock(channel_in=CHANNELS[0], channel_out=CHANNELS[0], activation_fn=activation_fn),
+            nn.Conv2d(in_channels=CHANNELS[0], out_channels=3, kernel_size=1, bias=False)
+        )
+
+    def forward(self, *input: Any):
+        h = self.fc(input)
+        h = h.view(len(h), *self.deepest_shape)
+        return self.conv(h)
+
+
+class MVTecNet_Autoencoder(BaseModule):
+    """Full AutoEncoder network
+    
+    """
+    def __init__(self, input_shape: int, code_length: int, use_selectors: bool):
+        """Init Full AutoEncoder
+
+        Parameters
+        ----------
+        input_shape : Tensor
+            Shape of input data
+        code_length : int
+            Latent code size
+        use_selectors : bool
+            True (False) if the model has (not) to use Selectors modules
+
+        """
+        super().__init__()
+
+        # Shape of input data needed by the Decoder
+        self.input_shape = input_shape
+        
+        # Build Encoder
+        self.encoder = MVtecEncoder(
+                                input_shape=input_shape,
+                                code_length=code_length,
+                                idx_list_enc=[],
+                                use_selectors=use_selectors
+                            )
+
+        last_depth, deepest_shape = self.encoder.get_depths_info()
+
+        # Build Decoder
+        self.decoder = MVtecDecoder(
+                                code_length=code_length,
+                                deepest_shape=deepest_shape,
+                                last_depth=last_depth,
+                                output_shape=input_shape
+                            )
+
+    def forward(self, *input: Any):
+        z = self.encoder(input)
+        x_r = self.decoder(z)
+        x_r = x_r.view(-1, *self.input_shape)
+        return x_r
